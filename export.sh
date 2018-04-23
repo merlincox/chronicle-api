3adae02c00976d3aa46a4ecb95e4c17f7c4e2b8e
Merlin Cox <merlin.cox@denma.digital>
2018-04-23 14:21:44 +0100

    Change export to take release arg and extract the API id using jq

--- export.sh
+++ export.sh
@@ -2,14 +2,32 @@
 
 set -euo pipefail
 
-api_id=zhs7h308vg
-stage_name=1804221651
+release_pattern="^[a-z0-9]+$"
+
+if [ $# -lt 1 ] ; then
+
+    echo "USAGE $0 release-name [stage-name]"
+    exit 1
+fi
+
+if [[ ! $1 =~ $release_pattern ]] ; then
+
+    echo "Invalid release-name: $1 does not match $release_pattern regex"
+    exit 1
+fi
+
+release=$1
 package=models
 package_dir=models
+export_dir=export
+
+# Get the API id from the API name using jq command-line json tool. See https://stedolan.github.io/jq
+
+api_id=$( aws apigateway get-rest-apis | jq  -r '.items[] | select(.name == "Chronicle-API-'${release}'") | .id' )
 
 # Export a JSON-format Swagger API definition from the AWS Gateway API
 
-aws apigateway get-export --rest-api-id $api_id  --stage-name $stage_name --export-type swagger export/$stage_name.json
+aws apigateway get-export --rest-api-id $api_id  --stage-name $release --export-type swagger $export_dir/$release.json
 
 # The schema-generator executable can be created from here: https://github.com/merlincox/generate
 # It generates a Go source file of struct declarations from the Swagger API definition file
@@ -18,6 +36,6 @@ schema_generator=$(which schema-generator)
 
 if [ ! -z "$schema_generator" ]; then
 
-    $schema_generator -p $package -nsk export/$stage_name.json > export/$stage_name.go
+    $schema_generator -p $package -nsk $export_dir/$release.json > $export_dir/$release.go
 
 fi
